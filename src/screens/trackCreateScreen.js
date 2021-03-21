60151cd50c5792907403dd68ee7b3d333aa58b0e
cipher-tech <nickchibuikem@gmail.com>
2021-03-21 19:28:32 +0100

    added mock location

--- src/screens/trackCreateScreen.js
+++ src/screens/trackCreateScreen.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import { requestPermissionsAsync, st } from 'expo-location'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet} from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Map from '../components/map'
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null)
+
+    const startWatching = async () => {
+        try {
+            let {status} =  await requestPermissionsAsync()
+            if(status !== 'granted'){
+                setErr("Please Enable Tracking")
+            }
+        } catch (error) {
+            setErr(error)
+        }
+    }
+    useEffect(() => {
+        startWatching()
+    }, [])
     return (
         <SafeAreaView>
             <Text h3>TrackCreate Screen</Text>
             <Map />
+            {err ? <Text>{err}</Text> : null}
         </SafeAreaView>
     )
 }
